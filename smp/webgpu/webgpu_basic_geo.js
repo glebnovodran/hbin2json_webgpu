20c23375fc2ea05abc44d6b9b50767ca9944875a
Glib Novodran <novodran@gmail.com>
2024-03-19 15:06:47 +0100

    Basic WebGPU sample - work in progress

--- smp/webgpu/webgpu_basic_geo.js
+++ smp/webgpu/webgpu_basic_geo.js
@@ -1,7 +1,53 @@
+class GPUWk {
+
+}
+
+g_wk = new GPUWk();
+
+async function initGPU() {
+	console.info("initGPU...");
+}
+
+function createModel(json) {
+}
+
+function update() {
+	const cam = scene.cam;
+	cam.eye.set(0.0, 0.2, 1.0);
+	cam.tgt.set(0.0, 0.25, 0.0);
+	cam.update();
+	g_mdlMtx = mdegy(g_y_degrees);
+	g_y_degrees -= 0.5;
+}
+
+function loop() {
+	update();
+	requestAnimationFrame(loop);
+}
+
+async function start() {
+	scene.printFiles();
+	await initGPU();
+
+	let mdlSrc = scene.files["basic.json"];
+	if (mdlSrc) {
+		let mdlJson = JSON.parse(mdlSrc);
+		if (mdlJson.dataType == "geo") {
+			g_mdlData = mdlJson;
+			createModel(mdlJson);
+		}
+	}
+
+	requestAnimationFrame(loop);
+}
+
+
 function main() {
 	console.clear();
 	scene.init("canvas", "webgpu");
 	scene.load([
+		"basic_vert.wgsl",
+		"basic_frag.wgsl",
 		"basic.json"
 	], start);
 }
